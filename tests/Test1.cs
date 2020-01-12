961a8a5f16806a30de4551477e4016e232561a33
Maria-Yemelianova <yemelianova.me@gmail.com>
2020-01-12 15:39:59 +0200

    setup serilog, basic auth

--- tests/Test1.cs
+++ tests/Test1.cs
@@ -2,7 +2,10 @@ using System;
 using base_automation_project.utils;
 using base_automation_project.utils.Models;
 using base_automation_project.utils.WebDriverBuilder;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -13,6 +16,7 @@ namespace base_automation_project.tests
 
         public Test1(TestFixture fixture) : base(fixture)
         {
+            
         }
 
         // do the checks here
@@ -21,7 +25,11 @@ namespace base_automation_project.tests
         [Fact]
         public void DemoTest()
         {
-            Logger.Error("test msg");
+                WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(30));
+                
+
+                Logger.Error("test msg");
+
         }
     }     
 }
